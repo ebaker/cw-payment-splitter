d6ad7000a98a7d0cf09dc603871e5afc7b4defd1
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-10-02 16:35:31 -0700

    Update page name to cw-payment-splitter.

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use cw_splits::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw_payment_splitter::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
